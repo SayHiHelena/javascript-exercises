3ca5f6df535e0de9d479baffd7fd858b4eaf9d15
SayHiHelena <cuiziyuan99@gmail.com>
2022-12-11 22:06:42 +1100

    Create solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
 const removeFromArray = function() {
+// const removeFromArray = function(ary, element) {
+//     // take 1 element to be removed from the array
+//     let removeIndex;
+//     for(let i = 0; i < ary.length; i++) {
+//         if(ary[i] == element) {
+//             removeIndex = i;
+//         }
+//     }
+//     if(removeIndex != null)
+//         ary.splice(removeIndex, 1);
+//     return ary;
 
+
+    // take multiple parameters using "arguments"
+    let targets = Array.from(arguments);
+    // separate the array and target elements
+    let ary = targets.shift();
+
+    for(let i = 0; i < targets.length; i++) {
+        // remove the target element and update the array
+        ary = ary.filter(element => element !== targets[i])
+    }
+
+    return ary;
 };
 
 // Do not edit below this line
